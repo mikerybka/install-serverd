7bf663ea09a2a288a8cb85dfc25a800901aa01fe
Mike Rybka <merybka@gmail.com>
2025-04-28 17:04:42 -0400

    Add logging

--- main.go
+++ main.go
@@ -9,29 +9,36 @@ import (
 
 func main() {
 	// Install serverd
+	fmt.Printf("Installing serverd...")
 	cmd := exec.Command("go", "install", "github.com/mikerybka/serverd@latest")
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done.")
 
 	// Locate executable
+	fmt.Printf("Locating exe...")
 	cmd = exec.Command("which", "serverd")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		panic(err)
 	}
 	path := strings.TrimSpace(string(out))
+	fmt.Println(" Done.")
 
 	// Get tailsacale IP
+	fmt.Printf("Getting tailscale IP...")
 	cmd = exec.Command("tailscale", "ip", "--4")
 	out, err = cmd.CombinedOutput()
 	if err != nil {
 		panic(err)
 	}
 	ip := strings.TrimSpace(string(out))
+	fmt.Println(" Done.")
 
 	// Write systemd unit
+	fmt.Printf("Writing systemd unit...")
 	err = os.WriteFile("/etc/systemd/system/serverd.service", fmt.Appendf(nil, `[Unit]
 Description=serverd
 After=network.target
@@ -47,16 +54,21 @@ WantedBy=multi-user.target
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done.")
 
 	// Start and enable service
+	fmt.Printf("Reloading systemd...")
 	cmd = exec.Command("systemctl", "daemon-reload")
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done.")
+	fmt.Printf("Starting serverd...")
 	cmd = exec.Command("systemctl", "enable", "--now", "serverd")
 	err = cmd.Run()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(" Done.")
 }
